ea7ee2ae8771e4f8ef842c8c7df1440cd5803308
Supattana-S <supattana.sue@gmail.com>
2022-06-21 14:22:06 +0700

    finish ToDoInput

---
+++ src/components/ToDoInput.js
@@ -0,0 +1,22 @@
+import Button from "./ui/Button";
+
+function ToDoInput() {
+  return (
+    <div class="input-group">
+      <input
+        type="text"
+        class="form-control shadow"
+        placeholder="Enter new todo"
+        aria-label="Recipient's username with two button addons"
+      />
+      <Button color="success">
+        <i className="fa-solid fa-plus" />
+      </Button>
+      <Button color="outline-secondary">
+        <i className="fa-solid fa-xmark" />
+      </Button>
+    </div>
+  );
+}
+
+export default ToDoInput;
---
+++ src/components/ui/Button.js
@@ -0,0 +1,10 @@
+function Button(props) {
+  const classes = `btn btn-${props.color || "primary"} shadow-none`;
+  return (
+    <button class={classes} type="button">
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
