01e732f0db1f687f9b8b7ad14c3bf029b31df135
Supattana-S <supattana.sue@gmail.com>
2022-06-22 08:36:30 +0700

    Done PageLimit section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ToDoInput from "./components/ToDoInput";
 import Filter from "./components/filter/Filter";
+import PageLimit from "./components/page-limit/PageLimit";
 
 function App() {
   return (
     <div className="container max-w-xs pt-5">
       <ToDoInput />
       <Filter />
+      <PageLimit />
     </div>
   );
 }
