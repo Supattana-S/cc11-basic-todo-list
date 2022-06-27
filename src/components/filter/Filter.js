58346495cf67844b9af192ce56e3e4a1d42dfeb5
Supattana-S <supattana.sue@gmail.com>
2022-06-27 14:54:00 +0700

    Search Feature: send searchTerm, changeSearchTerm to filter Search component

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -4,8 +4,11 @@ import FilterStatus from "./FilterStatus";
 function Filter(props) {
   return (
     <div className="mt-4 d-flex">
-      <FilterSearch />
-      <FilterStatus changeSearchStatus={props.changeSearchStatus} searchStatus={props.searchStatus}/>
+      <FilterSearch changeSearchTerm={props.changeSearchTerm} searchTerm={props.searchTerm} />
+      <FilterStatus
+        changeSearchStatus={props.changeSearchStatus}
+        searchStatus={props.searchStatus}
+      />
     </div>
   );
 }
