4acb52b99f009b653a88f399fb3d7da5a2827c3b
Supattana-S <supattana.sue@gmail.com>
2022-07-04 19:52:31 +0700

    get data from server(cc9 todolists): can't use this way

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 import ToDoInput from "./components/todo-list/ToDoInput";
 import Filter from "./components/filter/Filter";
 import PageLimit from "./components/page-limit/PageLimit";
 import ToDoList from "./components/todo-list/ToDoList";
 import Pagination from "./components/pagination/Pagination";
 
-// const todo = {
-//   title: "Homework",
-//   completed: true,
-// };
-
-// const todolist = [
-//   { title: "Sport", completed: true, id: 1 },
-//   { title: "Gaming", completed: false, id: 3 },
-//   { title: "Group Project", completed: true, id: 2 },
+// const initialTodoList = [
+//   { title: "Sport", completed: true, id: uuidv4() },
+//   { title: "Gaming", completed: false, id: uuidv4() },
+//   { title: "Entertain", completed: true, id: uuidv4() },
 // ];
+let initialTodoList;
 
-const initialTodoList = [
-  { title: "Sport", completed: true, id: uuidv4() },
-  { title: "Gaming", completed: false, id: uuidv4() },
-  { title: "Entertain", completed: true, id: uuidv4() },
-];
-//uuidv4() will random unique string
-
-// console.log(initialTodoList);
+axios.get("http://localhost:8080/todos").then((res) => {
+  initialTodoList = res.data.todos;
+  console.log(initialTodoList);
+});
 
 function App() {
   // create state in app to pass through state to children component in App.js
@@ -93,8 +86,10 @@ function App() {
   // }
 
   const filteredTodoList = todoList.filter(
-    (el) => (searchStatus === null || el.completed === searchStatus) && el.title.toLowerCase().includes(searchTerm.toLowerCase()));
-
+    (el) =>
+      (searchStatus === null || el.completed === searchStatus) &&
+      el.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className="container max-w-xs pt-5">
