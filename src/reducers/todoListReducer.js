848007b45c5033bc8e987903deca70443f3129b7
Supattana-S <supattana.sue@gmail.com>
2022-07-07 14:40:27 +0700

    Clarify all action that will happen in this web

--- src/reducers/todoListReducer.js
+++ src/reducers/todoListReducer.js
@@ -1,3 +1,11 @@
+// Action TYPE
+// 1. CREAT_TODO
+// 2. DELETE_TODO
+// 3. TOGGLE_TODO_STATUS
+// 4. UPDATE_TODO_STATUS
+// 5. CHANGE_SEARCH_TERM
+// 6. CHANGE_SEARCH_STATUS
+
 export const INITIAL_TODO_LIST = {
   todoList: [],
   searchTerm: "",
