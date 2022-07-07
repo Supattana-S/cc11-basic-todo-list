20633f641ba03aef549976bd00addf481da9a45a
Supattana-S <supattana.sue@gmail.com>
2022-07-07 14:29:57 +0700

    comment to prepare for using reducer

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -7,47 +7,47 @@ function TodoContextProvider(props) {
   const [todoList, setTodoList] = useState([]);
 
   const createTodo = (title) => {
-    axios
-      .post("http://localhost:8080/todos", {
-        title,
-        completed: false,
-      })
-      .then((res) => {
-        const newTodo = res.data.todo;
-        const newTodoList = [newTodo, ...todoList];
-        setTodoList(newTodoList);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // axios
+    //   .post("http://localhost:8080/todos", {
+    //     title,
+    //     completed: false,
+    //   })
+    //   .then((res) => {
+    //     const newTodo = res.data.todo;
+    //     const newTodoList = [newTodo, ...todoList];
+    //     setTodoList(newTodoList);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   };
 
   const removeTodo = (id) => {
-    axios
-      .delete(`http://localhost:8080/todos/${id}`)
-      .then(() => {
-        const idx = todoList.findIndex((el) => el.id === id);
-        if (idx !== -1) {
-          const cloneTodoList = [...todoList];
-          cloneTodoList.splice(idx, 1);
-          setTodoList(cloneTodoList);
-        }
-      })
-      .catch((err) => console.log(err));
+    // axios
+    //   .delete(`http://localhost:8080/todos/${id}`)
+    //   .then(() => {
+    //     const idx = todoList.findIndex((el) => el.id === id);
+    //     if (idx !== -1) {
+    //       const cloneTodoList = [...todoList];
+    //       cloneTodoList.splice(idx, 1);
+    //       setTodoList(cloneTodoList);
+    //     }
+    //   })
+    //   .catch((err) => console.log(err));
   };
 
   const updateTodo = (newValue, id) => {
-    axios
-      .put("http://localhost:8080/todos/" + id, newValue)
-      .then(() => {
-        const idx = todoList.findIndex((el) => el.id === id);
-        if (idx !== -1) {
-          const cloneTodoList = [...todoList];
-          cloneTodoList[idx] = { ...cloneTodoList[idx], ...newValue };
-          setTodoList(cloneTodoList);
-        }
-      })
-      .catch((err) => console.log(err));
+    // axios
+    //   .put("http://localhost:8080/todos/" + id, newValue)
+    //   .then(() => {
+    //     const idx = todoList.findIndex((el) => el.id === id);
+    //     if (idx !== -1) {
+    //       const cloneTodoList = [...todoList];
+    //       cloneTodoList[idx] = { ...cloneTodoList[idx], ...newValue };
+    //       setTodoList(cloneTodoList);
+    //     }
+    //   })
+    //   .catch((err) => console.log(err));
   };
 
   useEffect(() => {
