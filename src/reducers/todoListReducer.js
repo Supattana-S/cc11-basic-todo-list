90e916b4595470383a58a6b226d1a57a6bf05aa0
Supattana-S <supattana.sue@gmail.com>
2022-07-07 14:37:38 +0700

    Create object to store all state in this todo

--- src/reducers/todoListReducer.js
+++ src/reducers/todoListReducer.js
@@ -1,3 +1,7 @@
-export const INITIAL_TODO_LIST = [];
+export const INITIAL_TODO_LIST = {
+  todoList: [],
+  searchTerm: "",
+  searchStatus: null,
+};
 
 export function todoListReducer(state, action) {}
